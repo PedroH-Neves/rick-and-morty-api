33ecaff12efe7f845a6274da23a955f2080e6d03
PedroH-Neves <PedroHDN710@gmail.com>
2023-08-12 02:54:19 -0400

    Add details css

--- src/components/CharDetails.js
+++ src/components/CharDetails.js
@@ -1,12 +1,28 @@
-import { useSelector } from "react-redux";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import '../styles/components/CharDetails.scss';
 
 const CharDetails = ({char}) => {
+
   return (
-    <div>
-      {char.name}
-    </div>
-  )
+      <>
+      <FontAwesomeIcon icon={faArrowLeft}>Arrow</FontAwesomeIcon>
+      <div className="detail-div">
+        <img src={char.image} />
+        <ul>
+          <li><span>Name: </span><span className="info">{char.name}</span></li>
+          <li><span>Status: </span><span className="info">{char.status}</span></li>
+          <li><span>Species: </span><span className="info">{char.species}</span></li>
+          <li><span>Type: </span><span className="info">{char.type === '' ? 'None' : char.type}</span></li>
+          <li><span>Gender: </span><span className="info">{char.gender}</span></li>        
+          <li><span>Origins: </span><span className="info">{char.origin.name}</span></li>
+          <li><span>Location: </span> <span className="info">{char.location.name}</span></li>
+        </ul>
+      </div>
+      </>
+    )
 }
+
 
 
 export default CharDetails;
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -4,30 +4,14 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 const Details = () => {
-  const [char, setChar] = useState({})
   const { id } = useParams();
 
   const characters = useSelector(state => state.charactersReducers.characters);
   const charDetails = (characters.filter(char => char.id === Number(id))[0])
 
-  useEffect(() => {
-    setChar(charDetails)
-  }, [])
-
-  const renderChar = {
-    id: char.id,
-    name: char.name,
-    status: char.status,
-    location: char.location,
-    species: char.species,
-    gender: char.gender,
-    origin: char.origin,
-    image: char.image,
-  }
-
   return (
     <div>
-      <CharDetails char={renderChar}/>
+      <CharDetails char={charDetails}/>
     </div>
   )
 }
