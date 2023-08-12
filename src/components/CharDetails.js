8cff4d3b7cd7ddca27ea204667254eb8409c6943
PedroH-Neves <PedroHDN710@gmail.com>
2023-08-12 10:12:33 -0400

    Add back home function

--- src/components/CharDetails.js
+++ src/components/CharDetails.js
@@ -1,12 +1,20 @@
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import '../styles/components/CharDetails.scss';
+import { useNavigate } from "react-router-dom";
 
-const CharDetails = ({char}) => {
+const CharDetails = ({ char }) => {
+  const navigate = useNavigate();
+
+  const backHome = () => {
+    navigate('/');
+  }
 
   return (
-      <>
-      <FontAwesomeIcon icon={faArrowLeft}>Arrow</FontAwesomeIcon>
+    <>
+      <div className="back-arrow" onClick={backHome}>
+        <FontAwesomeIcon icon={faAngleLeft}>Arrow</FontAwesomeIcon>
+      </div>
       <div className="detail-div">
         <img src={char.image} />
         <ul>
@@ -14,13 +22,13 @@ const CharDetails = ({char}) => {
           <li><span>Status: </span><span className="info">{char.status}</span></li>
           <li><span>Species: </span><span className="info">{char.species}</span></li>
           <li><span>Type: </span><span className="info">{char.type === '' ? 'None' : char.type}</span></li>
-          <li><span>Gender: </span><span className="info">{char.gender}</span></li>        
+          <li><span>Gender: </span><span className="info">{char.gender}</span></li>
           <li><span>Origins: </span><span className="info">{char.origin.name}</span></li>
           <li><span>Location: </span> <span className="info">{char.location.name}</span></li>
         </ul>
       </div>
-      </>
-    )
+    </>
+  )
 }
 
 
